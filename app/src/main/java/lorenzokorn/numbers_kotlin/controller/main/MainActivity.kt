7d19fecdff9867cdc9a6b2928cdd54b086c709e8
lorenzo korn <lkorn.9520@gmail.com>
2019-11-18 10:08:54 +0100

    added logic in the main activity

--- app/src/main/java/lorenzokorn/numbers_kotlin/controller/main/MainActivity.kt
+++ app/src/main/java/lorenzokorn/numbers_kotlin/controller/main/MainActivity.kt
@@ -1,25 +1,56 @@
 package lorenzokorn.numbers_kotlin.controller.main
 
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import lorenzokorn.numbers_kotlin.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        initView()
+        initViewModel()
+    }
+
+    private fun initView() {
+        fab.setOnClickListener {
+            // get random number when fab is clicked
+            viewModel.getRandomTrivia()
         }
+    }
+
+    /**
+     * Initialize the mainActivityViewModel
+     */
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+
+        // Observe the viewModel (checks constantly for changes)
+        viewModel.trivia.observe(this, Observer {
+            // it points to the trivia object
+            tvTrivia.text = it?.text
+        })
+
+        // Observe the error message
+        viewModel.error.observe(this, Observer {
+            // it is the error
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
